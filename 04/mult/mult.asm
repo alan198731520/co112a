ac26d6107d56487d562d1d70dbb55aa745f650b3
alan198731520 <wheel910228@gmail.com>
2024-01-12 23:52:11 +0800

    all data

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R1
+    D = M		// load R1 into D
+
+    @i			// set the backwards counter to R1
+    M = D
+
+    @product		// product = 0 before program begins
+    M = 0
+
+(LOOP)			// while i > 0, keep looping
+    @i
+    D = M
+
+    @END		// otherwise, goto END
+    D;JEQ
+
+    @R0
+    D = M
+
+    @product		// add R0 to 'product'
+    M = D + M
+
+    @i			// reduce i
+    M = M - 1	
+
+    @LOOP		// restart the loop
+    0;JMP
+
+(END)
+    @product		// load 'product' to D
+    D = M
+  
+    @R2			// store result in R2
+    M = D
+
+    @END
+    0;JMP 		// infinite loop to prevent hacking
